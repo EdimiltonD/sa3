4ebad9723e1471f01ce072d6b82e81e2c80d6e64
EdimiltonD <edimilton.sobrinho@oi.net.br>
2022-08-30 14:06:19 -0300

    att login

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { Component, NgModule } from '@angular/core';
 import { Route, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../views/home/home.component';
+import { LoginComponent } from '../views/login/login.component';
 
 const Routes: Routes = [
-  {path:'', component: HomeComponent},
+  {path:"", component: HomeComponent},
+  {path:"Login",component: LoginComponent}
 ]
 
 
